a3f89fff20cde6d5a58eae95bd95a21af1f6add3
Matheus Cardoso <matheusjosecardoso@outlook.com>
2022-06-28 21:01:21 -0300

    Alterações aula 28-06

--- canvas.js
+++ canvas.js
@@ -1,25 +1,31 @@
 const canvas = document.getElementById('dino-game');
 const context = canvas.getContext('2d');
 
+//Definimos as variáveis
 let score;
 let highscore;
 let dino;
 let gravity;
+let obstacles = [];
 let enemies;
 let gameSpeed;
 let keys = {};
 
 // ArrowDown
 document.addEventListener("keydown", function(e) {
+    //Detectamos o evento de aperto de tecla (pra baixo)
     keys[e.code] = true;
+
 })
 
 // ArrowUp 
 document.addEventListener("keyup", function(e) {
+    //Detectamos o evento de aperto de tecla (pra cima)
     keys[e.code] = false;
 })
 
 class Dino {
+    //Criamos o elemento dinossauro
     constructor(x, y, w, h) {
         this.x = x
         this.y = y
@@ -33,6 +39,7 @@ class Dino {
         this.jumpTimer = 0;
     }
 
+    //Definimos o estilo do elemento
     Draw() {
         context.beginPath();
         context.fillStyle = "#000";
@@ -41,13 +48,25 @@ class Dino {
     }
 
     Animate() {
-        if(keys['ArrowUp']) {
+        //Executará a ação do pulo caso seja pressionada a tela SETA P/ CIMA ou ESPAÇO
+        if(keys['ArrowUp'] || keys['Space']) {
             this.Jump();
+            console.log('jump');
         } else {
             this.jumpTimer = 0;
         }
 
-        if(this.y + this.h < canvas.height) {
+        if (keys['ShiftLeft'] || keys['KeyS']) {
+            this.h = this.originalHeight / 2;
+        } else {
+            this.h = this.originalHeight;
+        }
+
+        this.y += this.dy; 
+
+        // Animação realizada caso a condição seja válida (da posição que está no Y e na altura
+        //Caso seja invalido significa que ele ta no chao
+        if (this.y + this.h < canvas.height) {
             this.dy += gravity;
             this.grounded = false;
         } else {
@@ -55,19 +74,69 @@ class Dino {
             this.grounded = true;
             this.y = canvas.height - this.h;
         }
-
-        this.y += this.dy; 
+    
 
         this.Draw();
     }
 
+    //Verificamos se está no chão e validamos o tempo do pulo
     Jump() {
         if(this.grounded && this.jumpTimer == 0) {
             this.jumpTimer = 1;
-            this.dy = - this.jumpForce;
+            this.dy = -this.jumpForce;
+        } else if (this.jumpTimer > 0 && this.jumpTimer < 15) {
+            this.jumpTimer++;
+            this.dy = -this.jumpForce - (this.jumpTimer / 50);
         }
     }
 }
+
+class Obstacle {
+    constructor (x, y, w, h, c) {
+        this.x = x;
+        this.y = y
+        this.w = w;
+        this.h = h;
+        this.c = c;
+
+        this.dx = -gameSpeed;
+    }
+
+    Update () {
+        this.x += this.dx;
+        this.Draw();
+        this.dx = -gameSpeed;
+    }
+
+    Draw () {
+        context.beginPath();
+        context.fillStyle = "#000";
+        context.fillRect(this.x, this.y, this.w, this.h);
+        context.closePath();
+    }
+}
+
+// Game Functions
+function SpawnObstacle() {
+    let size = RandomIntInRange(20, 70);
+    let type = RandomIntInRange(0, 1);
+    let obstacle = new Obstacle(canvas.width + size, canvas.height - size,
+        size, size, '#2484E4');
+
+    if (type == 1) {
+        obstacle.y -= dino.originalHeight - 10;
+    }
+
+    obstacles.push(obstacle);
+}
+
+SpawnObstacle();
+
+function RandomIntInRange(min, max) {
+    return Math.round(Math.random() * (max - min) + min);
+
+}
+
 
 function init() {
     canvas.width = window.outerWidth;
@@ -85,6 +154,9 @@ function init() {
 
     requestAnimationFrame(Update)
 }
+
+let initialSpawnTimer = 200;
+let spawnTimer = initialSpawnTimer;
 
 function Update() {
     requestAnimationFrame(Update)
